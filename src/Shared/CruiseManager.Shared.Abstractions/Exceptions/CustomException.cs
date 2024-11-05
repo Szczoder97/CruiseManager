0bc3f2b526bd441635ca2d8f9474bd4dec45760f
Szczodry Szczoder <pawelszczo@gmail.com>
2024-11-05 19:10:40 +0200

    custom exception abstr

---
+++ src/Shared/CruiseManager.Shared.Abstractions/Exceptions/CustomException.cs
@@ -0,0 +1,8 @@
+namespace CruiseManager.Shared.Abstractions.Exceptions;
+
+public abstract class CustomException : Exception
+{
+    protected CustomException(string message) :base(message)
+    {
+    }
+}
